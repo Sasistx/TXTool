66378762fe171671e8f8d28c6fbe4932b2ca8571
gaotianxiang <gaotianxiang@zhihu.com>
2018-09-06 11:39:18 +0800

    fix

---
+++ lib/tx/ArgParser.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+class ArgParser
+    def gitBrIsValid(path)
+        if path.length >=3 && path != "dev"
+            return true
+        else 
+            return false
+        end
+    end
+
+    def gitBrInvalidMessage(path)
+        puts "🚫 git br named #{path} is inValid, you can try it manually"
+    end
+end
+
